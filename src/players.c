0b8960b47109d670315b6272d9b1ef7a372b7a48
unknown <112853869+aleor-kth@users.noreply.github.com>
2023-04-06 12:52:35 +0200

    add missing stdlib to players.c

--- src/players.c
+++ src/players.c
@@ -1,15 +1,17 @@
+#include <stdlib.h>
+
 typedef struct{
     float posX, posY;
     float velocityY, velocityX;
     float accelerationY, accelerationX;
-}Player;
+} Player;
 
 
 Player* createPLayer(int x, int y){
     Player *pPlayer = malloc(sizeof(Player));
     pPlayer->posX = x;
     pPlayer->posX = y;
-    pPlayer->velocityX = 0; 
+    pPlayer->velocityX = 0;
     pPlayer->velocityY = 0;
     pPlayer->accelerationY = 0;
     pPlayer->accelerationX = 0;
