22e681f8af4cd596f23209ae1dc75cea8894ff26
Beatrice Poriazov <bettypori@gmail.com>
2023-04-06 11:24:08 +0200

    Changed reasourses/background.png to assets/background.png

--- src/platforms.c
+++ src/platforms.c
@@ -3,7 +3,7 @@
 #include "platforms.h"
 
 SDL_Texture * initBackground(SDL_Window *pWindow, SDL_Renderer *pRenderer){
-    SDL_Surface *pSurface = IMG_Load("resources/background.png");
+    SDL_Surface *pSurface = IMG_Load("assets/background.png");
     if(!pSurface){
         printf("Error: %s\n", SDL_GetError());
         SDL_DestroyRenderer(pRenderer);
