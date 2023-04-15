641d441b5804f8d99b0a77632ef2612eb86cd7ba
Amanda Bergström <amanda_bergstrom@hotmail.com>
2023-04-15 13:54:39 +0200

    Uppdaterat kommentarer som beskriver movePlayer funktionen

--- include/players.h
+++ include/players.h
@@ -5,7 +5,7 @@ typedef struct player Player;
 
 Player* createPlayer(int x, int y);
 void jumpPlayer(Player* pPlayer, SDL_Rect playerRect, int windowHeight, float platformY, float maxJumpHeight);
-void movePlayer (Player* pPlayer, SDL_Rect playerRect, bool left, bool right, int width);
+void movePlayer (Player* pPlayer, SDL_Rect playerRect, bool left, bool right, int width); //Funktion som hanterar att spelaren kan rörar sig i sidled med tangenttryck
 //void platformCollidePlayer(Player player, SDL_Rect playerRect, SDL_Rect platforms[], int nrOfPlatforms, float *pPlatformHeight, float *pMaxJumpHeight);
 void platformCollidePlayer(Player* pPlayer, SDL_Rect playerRect, SDL_Rect platform, int nrOfPlatforms, float* pPlatformY, float* pMaxJumpHeight);
 void updatePlayer(Player* pPlayer, SDL_Rect* pPlayerRect);
