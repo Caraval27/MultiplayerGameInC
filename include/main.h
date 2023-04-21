c68d03bd26ce7272c912d944542f986690ae847c
unknown <112853869+aleor-kth@users.noreply.github.com>
2023-04-21 13:07:11 +0200

    add libraries.h

--- include/main.h
+++ include/main.h
@@ -1,14 +1,6 @@
 #pragma once
 
-#include <stdio.h>
-#include <stdbool.h>
-#include <time.h>
-#include <string.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_net.h>
-#include <SDL2/SDL_mixer.h>
+#include "../include/libraries.h"
 #include "../include/world.h"
 #include "../include/fileManagement.h"
 #include "../include/players.h"
